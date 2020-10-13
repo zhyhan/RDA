f633aa8dc66b0a8c5b9cf113b06db218afa406eb
zhyhan <hanzy@outlook.com>
2020-10-13 15:31:12 +0800

    add ood noisy

--- data/noisify.py
+++ data/noisify.py
@@ -4,8 +4,33 @@ from numpy.testing import assert_array_almost_equal
 from imgaug import augmenters as iaa
 from PIL import Image
 import random
-import cv2
 import tqdm
+import glob
+
+def listing_file(domains, data_dir, save_dir):
+    for d in domains:
+        str_labels, num_labels = [], []
+        file_dir = data_dir + d + '/*/*'
+        save_file = save_dir + d + '.txt'
+        img_files = glob.glob(file_dir, recursive=True)
+        label, first = 0, True
+        for img in img_files:
+            str_label = img.split('/')[7]
+            if first:
+                num_labels.append(label)
+                str_labels.append(str_label)
+                first = False
+            else:
+                if str_label not in str_labels:
+                    label += 1
+                    str_labels.append(str_label)
+                    num_labels.append(label)
+                else:
+                    num_labels.append(label)
+        with open(save_file,'w') as f:
+            for i, img in enumerate(img_files):
+                f.write('{} {}\n'.format(img, num_labels[i]))
+
 
 def multiclass_noisify(y, T, random_state=0):
     """
@@ -84,63 +109,57 @@ def sp_blur_noise(image):
     output = Image.fromarray(output)
     return output
 
+def corrupt_image(file_dir):
+    for path in tqdm.tqdm(file_dir):
+        save_path = path.split('.')[0] + '_corrupted.jpg'
+        if not os.path.isfile(save_path):
+            image = pil_loader(path)
+            image = sp_blur_noise(image)
+            image.save(save_path)
+    print('complete corrupting images!')
+
+#def ood_noise():
+
 
 if __name__ == '__main__':
-    # listing source files into txt if not exists.
-    listing_file = False
-    if listing_file:
-        import glob
+
+    dataset = 'office-31' # or office-home
+    if dataset is 'office-home':
+        class_number = 65
+        data_files = ['Office-home/Art.txt', 'Office-home/Clipart.txt', 'Office-home/Product.txt', 'Office-home/Real_world.txt']
         domains = ['Art', 'Clipart', 'Product', 'Real_world']
         data_dir = '/data1/hanzhongyi/datasets/office/office-home/'
         save_dir = 'Office-home/'
-        for d in domains:
-            str_labels, num_labels = [], []
-            file_dir = data_dir + d + '/*/*'
-            save_file = save_dir + d + '.txt'
-            img_files = glob.glob(file_dir, recursive=True)
-            label, first = 0, True
-            for img in img_files:
-                str_label = img.split('/')[7]
-                if first:
-                    num_labels.append(label)
-                    str_labels.append(str_label)
-                    first = False
-                else:
-                    if str_label not in str_labels:
-                        label += 1
-                        str_labels.append(str_label)
-                        num_labels.append(label)
-                    else:
-                        num_labels.append(label)
-            with open(save_file,'w') as f:
-                for i, img in enumerate(img_files):
-                    f.write('{} {}\n'.format(img, num_labels[i]))
+    elif dataset is 'office-31':
+        class_number = 31
+        data_files = ['Office-31/webcam.txt', 'Office-31/dslr.txt', 'Office-31/amazon.txt']
+        domains = ['webcam', 'dslr', 'amazon']
+        data_dir = '/data1/hanzhongyi/datasets/office/office-31/'
+        save_dir = 'Office-31/'
+    else:
+        raise Exception("Sorry, unsupported dataset")
 
-    #data_files = ['Office-home/Art.txt', 'Office-home/Clipart.txt', 'Office-home/Product.txt', 'Office-home/Real_world.txt']
-    data_files = ['Office-31/webcam.txt', 'Office-31/dslr.txt', 'Office-31/amazon.txt']
-    #class_number = 31
-    class_number = 65
-    noisy_rate = [0.2,0.4,0.6,0.8]
-    noisy_type = ['uniform', 'pair']
-    corrupt_image = True
-    for data_file in data_files:
+    listing_file = False
+    corrupt_image = False
+
+
+    #listing source files of images into text if not exists.
+    if listing_file:
+        listing_file(domains, data_dir, save_dir)
+    
+    for data_file in data_files: #Read text files
         with open(data_file, 'r') as f:
             file_dir, label = [], []
             for i in f.read().splitlines():
                 file_dir.append(i.split(' ')[0])
                 label.append(int(i.split(' ')[1]))
-
         if corrupt_image:
-            for path in tqdm.tqdm(file_dir):
-                save_path = path.split('.')[0] + '_corrupted.jpg'
-                if not os.path.isfile(save_path):
-                    image = pil_loader(path)
-                    image = sp_blur_noise(image)
-                    image.save(save_path)
-            print('complete corrupting images!')
+            corrupt_image(file_dir)
 
-        #noisy label
+        #Todo: noisy label
         """
+        noisy_rate = [0.2,0.4,0.6,0.8]
+        noisy_type = ['uniform', 'pair']
         for tp in noisy_type:
             for rate in noisy_rate:
                 if tp is 'pair':
@@ -154,7 +173,7 @@ if __name__ == '__main__':
                     for i, d in enumerate(file_dir):
                         f.write('{} {} {}\n'.format(d, label_noisy[i], label[i]))
 
-                #save split noisy and clean data for validate the idea
+                '''split data into noisy and clean data for validating the idea'''
                 #save_clean_file = data_file.split('.')[0] + '_{}_noisy_{}_true.txt'.format(tp, rate)
                 #save_noisy_file = data_file.split('.')[0] + '_{}_noisy_{}_false.txt'.format(tp, rate)
                 #with open(save_clean_file,'w') as f:
@@ -166,7 +185,9 @@ if __name__ == '__main__':
                 #                ff.write('{} {}\n'.format(d, label_noisy[i]))
 
         print('complete corrupting labels!')
-        #noisy feature
+        """
+        """
+        #Todo: noisy feature
         noisy_feature_rate = [0.1,0.2,0.3,0.4,0.6,0.8]
         for rate in noisy_feature_rate:
             save_file = data_file.split('.')[0] + '_feature_noisy_{}.txt'.format(rate)
@@ -180,8 +201,37 @@ if __name__ == '__main__':
                     f.write('{} {}\n'.format(d, label[i]))
             print('feature noise true/given {}/{}'.format(float(num)/len(file_dir), rate))
         print('complete corrupting features!')
+        """
+        #Todo: introduce out-of-distribution (ood) noise
+        ood_dataset = "Bing-Caltech/Caltech.txt" #Introduce other dataset for providing 
+        with open(ood_dataset, 'r') as f:
+            ood_file_dir, ood_label = [], []
+            for i in f.read().splitlines():
+                ood_file_dir.append(i.split(' ')[0])
+                ood_label.append(int(i.split(' ')[1]))
+        ood_noisy_rate = [0.1,0.2,0.3,0.4,0.6,0.8]
+        for rate in ood_noisy_rate:
+            save_file = data_file.split('.')[0] + '_ood_noisy_{}.txt'.format(rate)
+            #know every class number and add %rate
+            num_per_class = np.zeros(class_number)
+            for i in label:
+                num_per_class[i] += 1
+            #Todo: compute num_per_class
+            num_per_class = np.ceil(num_per_class*(rate/(1-rate)))       
+            random.shuffle(ood_file_dir)
+            new_file_dir = ood_file_dir[:int(num_per_class.sum())]
+            new_label = []
+            for i, j in enumerate(num_per_class):
+                new_label.extend([i]*int(j))
+            
+            with open(save_file,'w') as f:
+                    for i, d in enumerate(file_dir):
+                        f.write('{} {}\n'.format(d, label[i]))
+                    for i, d in enumerate(new_file_dir):
+                        f.write('{} {}\n'.format(d, new_label[i]))
 
-        #mix noise: feature noise + label noise
+        """
+        #Todo mix: feature noise + label noise
         for rate in noisy_rate:
             rate = rate #for fair comparison with TCL
             feature_noisy_file = data_file.split('.')[0] + '_feature_noisy_{}.txt'.format(rate/2)
@@ -213,5 +263,5 @@ if __name__ == '__main__':
                                 f.write('{} {}\n'.format(d, label_noisy[i]))
                             else:
                                 ff.write('{} {}\n'.format(d, label_noisy[i]))
-
-            """
+        """ 
+        #Todo mix: feature noise + label noise + ood noise
